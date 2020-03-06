5c55f8d942936d8a31f2ad096bb84fc154838c67
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-03-06 13:44:38 +0900

    feat: colored

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,31 +5,32 @@ import { Arc } from './Arc'
 import { Global, jsx, css } from '@emotion/core'
 import { newRandGen, randNext, randRange } from 'fn-mt'
 
-const globalStyle = css`
-  * {
-      padding: 0;
-      margin: 0;
-    }
-html {
-      background-color: #333;
-      color: #eee;
-    }
-a {
-      color: #eee;
-      text-decoration: none;
-}
-a:hover {
-      background-color: #eee;
-      color: #333
-}
-`
+const globalStyle = css({
+  '*': {
+      padding: 0,
+      margin: 0,
+    },
+  html: {
+      backgroundColor: '#333',
+      color: '#eee',
+    },
+  a: {
+      color: '#eee',
+      textDecoration: 'none',
+},
+  'a:hover': {
+      backgroundColor: '#eee',
+      color: '#333',
+}})
 
 type Ring = {
+  key: number
   start: number
   now: number
   end: number
   rad: number
   color: string
+  rotCW: boolean
 }
 export const App: React.FC = () => {
   // Adjust center on the browser
@@ -60,35 +61,52 @@ export const App: React.FC = () => {
       const [_, g] = randNext(gen)
       gen = g
     }
-    const rads = [...Array(20).keys()].map(i => i * 30)
+    const rads = [...Array(20).keys()].map(i => i * 30).concat([...Array(20).keys()].map(i => i * 30)).concat([...Array(20).keys()].map(i => i * 30))
     let results: Ring[] = []
-    for (const rad of rads){
-      const [start, g1] = randRange(0, 360, gen)
-      const [end, g2] = randRange(0, 360, g1)
-      gen = g2
-      results.push({start, now: start, end, rad, color: "#f48"})
-    }
+    rads.forEach((rad, key) => {
+      const [n1, g1] = randRange(0, 360, gen)
+      const [n2, g2] = randRange(0, 360, g1)
+      const [hue, g3] = randRange(0, 360, g2)
+      const [rot, g4] = randRange(0, 2, g3)
+      gen = g4
+      let start =0
+      let end = 0
+      if(n1 < n2){
+        start = n1
+        end = n2
+      }else{
+        start = n2
+        end = n1
+      }
+      const color = hsvToRgbHex(hue, 1, 1)
+      const rotCW = rot == 0 ? false : true
+      results.push({key, start: start, now: start, end, rad, color, rotCW})
+    })
     return results
   })
 
-  // Animate state
+  // Animation state
   const [tick, setTick] = useState(0)
   const requestRef = React.useRef(0)
   const prevTimeRef = React.useRef(0)
+  const [active, setActive] = useState(true)
   
   const animate = (time: number) => {
     const deltaTime = time - prevTimeRef.current
-    setTick(prevCount => (prevCount + deltaTime * 1) % 5000)
-    setRings(rings.map(r => ({...r, now: (r.start + (r.star < r.end == 0 ? -1 : 1) * (r.end - r.start) * (tick / 5000)) % 360})))
+    if(active){
+      setTick(prevCount => (prevCount + deltaTime * 1))
+      setRings(rings.map(r => ({...r, now: (r.start + (r.rotCW ? 1 : -1) * (r.end - r.start) * (tick / 1000)) % 360})))
+    }
+    if(tick >= 1000){
+      setTick(1000)
+      setActive(false)
+    }
     prevTimeRef.current = time
     requestRef.current = requestAnimationFrame(animate)
   }
-  const [active, setActive] = useState(true)
   useEffect(() => {
-    if(active){
-      requestRef.current = requestAnimationFrame(animate)
-      return () => cancelAnimationFrame(requestRef.current)
-    }
+    requestRef.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(requestRef.current)
   }, [tick])
 
   return (
@@ -109,14 +127,45 @@ export const App: React.FC = () => {
 const renderRing = (rx: number, ry: number, r: Ring) =>{ 
   return(
     <Arc
-      key={r.rad}
+      key={r.key}
       rx={rx}
       ry={ry}
       rad={r.rad}
       start={r.start}
       end={r.now}
-      rot={r.rot}
       color={r.color}
+      rotCW={r.rotCW}
     />
   )
+}
+
+const hsvToRgbHex = (H: number , S: number, V:number): string => {
+    const C = V * S
+    const Hp = H / 60
+    const X = C * (1 - Math.abs(Hp % 2 - 1))
+
+    let [R, G, B] = [0, 0, 0]
+    if (0 <= Hp && Hp < 1){
+      [R,G,B]=[C,X,0]
+    }else if(1 <= Hp && Hp < 2){
+      [R,G,B]=[X,C,0]
+    }else if(2 <= Hp && Hp < 3){
+      [R,G,B]=[0,C,X]
+    }else if(3 <= Hp && Hp < 4){
+      [R,G,B]=[0,X,C]
+    }else if(4 <= Hp && Hp < 5){
+      [R,G,B]=[X,0,C]
+    }else if(5 <= Hp && Hp < 6){
+      [R,G,B]=[C,0,X]
+    }
+
+    const m = V - C;
+    [R, G, B] = [R+m, G+m, B+m]
+
+    R = Math.floor(R * 255)
+    G = Math.floor(G * 255)
+    B = Math.floor(B * 255)
+
+    const f = (x: number): string => ('00' + x.toString(16)).slice(-2)
+    return ('#' + f(R) + f(G) + f(B))
 }
--- src/components/Arc.tsx
+++ src/components/Arc.tsx
@@ -8,12 +8,12 @@ type Props = {
   rad: number
   start: number
   end: number
-  rot: number
   color: string
+  rotCW: boolean
 }
 
-export const Arc: React.FC<Props> = ({rx, ry, rad, start, end, color}) => {
-  const arc = convert(rx, ry, rad, start, end)
+export const Arc: React.FC<Props> = ({rx, ry, rad, start, end, color, rotCW}) => {
+  const arc = convert(rx, ry, rad, start, end, rotCW)
   const arcStr = `M ${arc.rx}, ${arc.ry} a ${arc.rad} ${arc.rad} ${arc.start} ${arc.largeArcFlag} ${arc.sweepFlag} ${arc.dx},${arc.dy}`
   return (
     <path
@@ -35,7 +35,7 @@ type ArcSet = {
   dy: number
 }
 
-const convert = (x: number, y: number, rad: number, start_: number, end_: number): ArcSet => {
+const convert = (x: number, y: number, rad: number, start_: number, end_: number, rotCW: boolean): ArcSet => {
   const end = end_ % 360
   const start= start_ % 360
   const rx = x + rad * Math.cos(start/360*2*Math.PI)
@@ -44,19 +44,25 @@ const convert = (x: number, y: number, rad: number, start_: number, end_: number
   const ry2 = y + rad * Math.sin(end/360*2*Math.PI)
   const dx = rx2 - rx
   const dy = ry2 - ry
-  const large = Math.abs(start - end) > 180
-  let largeArcFlag = 0 
-  let sweepFlag = start < end ? 1 : 0
-  if(!large && sweepFlag === 1){
-    largeArcFlag = 0
-  }else if(large && sweepFlag === 1){
+  let sweepFlag = rotCW ? 1 : 0
+  let largeArcFlag = 0
+  const pos = end - start > 0
+  if(rotCW && pos && Math.abs(end - start) > 180){
     largeArcFlag = 1
-  }else if(!large && sweepFlag === 0){
-    largeArcFlag = 1
-    sweepFlag = 1
-  }else if(large && sweepFlag === 0){
+  }else if(rotCW && pos){
     largeArcFlag = 0
-    sweepFlag = 1
+  }else if(rotCW && !pos && Math.abs(end - start) > 180){
+    largeArcFlag = 0
+  }else if(rotCW && !pos){
+    largeArcFlag = 1
+  }else if(!rotCW && pos && Math.abs(end - start) > 180){
+    largeArcFlag = 0
+  }else if(!rotCW && pos){
+    largeArcFlag = 1
+  }else if(!rotCW && !pos && Math.abs(end - start) > 180){
+    largeArcFlag = 1
+  }else if(!rotCW && !pos){
+    largeArcFlag = 0
   }
   return {rx, ry, rad, start, largeArcFlag, sweepFlag, dx, dy}
 }
